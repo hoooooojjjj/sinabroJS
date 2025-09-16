3d7f39996cfdbf2c1bdff0eae64aa57668cc5fe4
hoooooojjjj <rhj080471@gmail.com>
2025-09-16 13:21:36 +0900

    main.js 파일에 새로운 제목 요소(h2, h3) 추가 및 클래스 추가 기능 구현. 'Hi, I am hojun'이라는 문단을 삽입하고, 요소를 삽입하는 기능을 추가했습니다.

--- ep01/src/main.js
+++ ep01/src/main.js
@@ -1,5 +1,25 @@
-console.log("Hello World");
 document.body.innerHTML = "<h1>Hello World</h1>";
-const h1 = document.createElement("h2")
-h1.innerHTML = "Hello World2"
-document.body.appendChild(h1)
+const h2 = document.createElement("h2")
+const h3 = document.createElement('h3');
+h2.innerHTML = "Hello World2"
+h3.innerHTML = "Hello World3"
+document.body.appendChild(h2)
+document.body.prepend(h3)
+
+const addClass = (ele)=> {
+    ele.classList.add("title")
+    console.log(ele.className)
+}
+
+const insertBefore = (newNode, referenceNode)=>{
+  document.body.insertBefore(newNode, referenceNode);
+  console.log(referenceNode)
+}
+
+addClass(h2)
+
+const p = document.createElement('p');
+p.innerText = "Hi, I am hojun"
+document.body.insertBefore(h2, p);
+
+insertBefore(h2, p)
